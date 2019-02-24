8690f850572d885355047c481c84bf585014b655
artem-galas <artem.galas@gmail.com>
2019-02-24 16:55:18 +0200

    Add Part3 Northwind Base WebApp

---
+++ Part3/NorthwindWeb/Pages/Suppliers.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
+using NorthwindContextLib;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NorthwindWeb.Pages
+{
+    public class SuppliersModel : PageModel
+    {
+        private Northwind db;
+
+        public IEnumerable<string> Suppliers { get; set; }
+        [BindProperty]
+        public Supplier Supplier { get; set; }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                db.Suppliers.Add(Supplier);
+                db.SaveChanges();
+                return RedirectToPage("/suppliers");
+            }
+            return Page();
+        }
+
+        public SuppliersModel(Northwind injectedContext)
+        {
+            db = injectedContext;
+        }
+
+        public void OnGet()
+        {
+            ViewData["Title"] = "Northwind Web Site - Suppliers";
+            Suppliers = db.Suppliers.Select(s => s.CompanyName).ToArray();
+        }
+    }
+}
